fd90003b603c10061efb22809a31a479c240a26d
xingyun <xingyun_888888@sina.com>
2017-04-24 01:38:11 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data(){
+    return {
+
+    }
+  },
+  methods:{
+
+  },
+  mounted(){
+
+  }
+}
+</script>
---
+++ src/components/Login.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="content">
+    <header class="bar bar-nav">
+      <a class="icon icon-left pull-left"></a>
+      <a class="icon icon-refresh pull-right"></a>
+      <h1 class="title">登录</h1>
+    </header>
+    <div class="zm-container">
+      <div>
+        <img src="">
+      </div>
+      <form action="" method="">
+        <div class="zm-box">
+           <div class="zm-box-item">
+              <div>
+                <i class="icon icon-me"></i><input type="text" placeholder="请输入手机号" />
+              </div>
+              <div>
+                <i class="icon icon-me"></i><input type="text" placeholder="请输入密码" /><button type="button" class="send-code">发送验证码</button>
+              </div>
+              <div>
+                <i class="icon icon-me"></i><input type="text" placeholder="请输入图片验证码" /><button>验证码</button>
+              </div>
+              <div class="zm-line">
+                <b></b>
+                <b class="zm-dot"></b>
+                <b></b>
+              </div>
+              <div style="display:block;width:100%;color:#999;text-align:center;margin-top:0;">已有账号</div>
+             <button type="button" style="width:100%;margin-top:1rem;background:#539ffd;outline:0;border:0;padding:0.2rem;border-radius: 2px;">登录</button>
+             <div class="zm-form-link">
+               <a href="">免费注册</a>
+               <a href="">忘记密码</a>
+             </div>
+           </div>
+        </div>     
+      </form>
+    </div>   
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+
+    }
+  },
+  methods:{
+  
+  },
+  mounted(){
+
+  }
+}
+</script>
+<style scoped>
+</style>
